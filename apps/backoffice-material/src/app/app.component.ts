7953db9dae08221a8b7edf4f5cf5954cc71d6526
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-12-01 15:59:59 +0200

    Initial commit

---
+++ apps/backoffice-material/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'danylo.shamaiev-backoffice-panel-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'backoffice-material';
+}
