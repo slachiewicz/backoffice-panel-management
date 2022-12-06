8a021b65892efc1c7138afd4a440cb48691b55ea
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-12-06 10:50:48 +0200

    Reformat with Prettier

--- apps/backoffice-material/src/app/app.module.ts
+++ apps/backoffice-material/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
--- apps/backoffice-primeng/src/app/app.module.ts
+++ apps/backoffice-primeng/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from './app.routes';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
   ],
   providers: [],
   bootstrap: [AppComponent],
