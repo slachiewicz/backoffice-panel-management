3670200b0b47f32754f1e342cf49f1ca6a24a1c3
Danylo Shamaiev <danyloshamaiev@gmail.com>
2023-02-10 12:40:13 +0200

    [Nebular] Remove NbLoginComponent mentions from Login page

--- apps/backoffice-nebular/src/app/login/login.component.ts
+++ apps/backoffice-nebular/src/app/login/login.component.ts
@@ -1,8 +1,7 @@
 import {Component} from '@angular/core';
-import {NbLoginComponent} from '@nebular/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent extends NbLoginComponent {}
+export class LoginComponent {}
