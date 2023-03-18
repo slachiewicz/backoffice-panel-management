b884e97ab61dd02cdbf66f448b8c0b26146c25cd
Danylo Shamaiev <danyloshamaiev@gmail.com>
2023-03-18 18:41:09 +0200

    [Material] Material App setup progress

--- apps/backoffice-material/src/app/app.component.ts
+++ apps/backoffice-material/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'danylo.shamaiev_backoffice-panel-app-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'backoffice-material';
-}
+export class AppComponent {}
--- apps/backoffice-material/src/app/app.module.ts
+++ apps/backoffice-material/src/app/app.module.ts
@@ -3,15 +3,15 @@ import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {getStorage, provideStorage} from '@angular/fire/storage';
+import { MatSliderModule } from "@angular/material/slider";
 import {BrowserModule} from '@angular/platform-browser';
 import {sharedEnvironment} from 'shared';
 
 import {AppComponent} from './app.component';
-import {NxWelcomeComponent} from './nx-welcome.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -19,6 +19,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent],
