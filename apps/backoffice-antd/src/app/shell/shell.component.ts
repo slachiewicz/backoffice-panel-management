d927ef1ca35552b45a7a2e8b43c7f4259d8558fa
Danylo Shamaiev <danyloshamaiev@gmail.com>
2023-04-03 13:00:13 +0300

    [Ant Design] Add Log out logic

--- apps/backoffice-antd/src/app/shell/shell.component.ts
+++ apps/backoffice-antd/src/app/shell/shell.component.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {AuthService} from 'libs/shared/src/lib/services/auth.service';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
 })
-export class ShellComponent {}
+export class ShellComponent {
+  constructor(private readonly authService: AuthService) {}
+
+  public handleLogout() {
+    this.authService.logout();
+  }
+}
