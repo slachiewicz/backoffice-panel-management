3515cee364ab2e29e55c7b914717cef0023b1b3e
Danylo Shamaiev <danyloshamaiev@gmail.com>
2023-04-04 16:42:10 +0300

    [PrimeNG] Make everything lazy-loaded with protected routes

--- apps/backoffice-primeng/src/app/app.module.ts
+++ apps/backoffice-primeng/src/app/app.module.ts
@@ -3,65 +3,29 @@ import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {getStorage, provideStorage} from '@angular/fire/storage';
-import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MessageService} from 'primeng/api';
-import {AvatarModule} from 'primeng/avatar';
-import {ButtonModule} from 'primeng/button';
-import {CalendarModule} from 'primeng/calendar';
-import {CardModule} from 'primeng/card';
-import {ChartModule} from 'primeng/chart';
-import {CheckboxModule} from 'primeng/checkbox';
-import {DialogModule} from 'primeng/dialog';
-import {DividerModule} from 'primeng/divider';
-import {InputTextModule} from 'primeng/inputtext';
-import {MenuModule} from 'primeng/menu';
 import {RippleModule} from 'primeng/ripple';
-import {TableModule} from 'primeng/table';
-import {TagModule} from 'primeng/tag';
 import {ToastModule} from 'primeng/toast';
 import {sharedEnvironment} from 'shared';
 
 import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './app.routes';
-import {ClientsComponent} from './clients/clients.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {LoginComponent} from './login/login.component';
-import {ShellComponent} from './shell/shell.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ShellComponent,
-    DashboardComponent,
-    ClientsComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
+    RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
     provideFirebaseApp(() => initializeApp(sharedEnvironment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
-    InputTextModule,
-    CheckboxModule,
-    ButtonModule,
-    RippleModule,
-    ReactiveFormsModule,
     ToastModule,
-    CardModule,
-    AvatarModule,
-    ChartModule,
-    DividerModule,
-    TableModule,
-    DialogModule,
-    CalendarModule,
-    MenuModule,
-    TagModule,
+    RippleModule,
   ],
   providers: [MessageService],
   bootstrap: [AppComponent],
---
+++ apps/backoffice-primeng/src/app/clients/clients.module.ts
@@ -0,0 +1,32 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {ButtonModule} from 'primeng/button';
+import {CalendarModule} from 'primeng/calendar';
+import {CardModule} from 'primeng/card';
+import {DialogModule} from 'primeng/dialog';
+import {TableModule} from 'primeng/table';
+import {ClientsComponent} from './clients.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ClientsComponent,
+  },
+];
+
+@NgModule({
+  declarations: [ClientsComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    ReactiveFormsModule,
+    CardModule,
+    TableModule,
+    DialogModule,
+    CalendarModule,
+    ButtonModule,
+  ],
+})
+export class ClientsModule {}
