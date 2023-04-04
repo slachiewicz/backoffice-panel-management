e966f6b09e904744854ac6c626047d7270aed248
Danylo Shamaiev <danyloshamaiev@gmail.com>
2023-04-04 13:36:09 +0300

    [Ant Design] Add real user's name

--- apps/backoffice-antd/src/app/shell/shell.component.ts
+++ apps/backoffice-antd/src/app/shell/shell.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
 import {AuthService} from 'shared';
+import {User} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
 })
 export class ShellComponent {
+  public user$: Observable<User | null>;
+
   constructor(
     private readonly authService: AuthService,
     private readonly router: Router
-  ) {}
+  ) {
+    this.user$ = this.authService.user$;
+  }
 
   public handleLogout() {
     this.authService
